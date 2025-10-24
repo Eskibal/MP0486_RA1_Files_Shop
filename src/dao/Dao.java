22d33513bd355f5ea2b3522869e68d143b84450f
Eskibal <jeanpatrick.esquibal@gmail.com>
2025-10-24 18:14:57 +0200

    modified Dao and Shop and created DaoImplFile

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,12 +1,21 @@
 package dao;
 
 import model.Employee;
+import model.Product;
+
+import java.util.ArrayList;
 
 public interface Dao {
 	
 	public void connect();
-
-	public void disconnect();
+	
+	public ArrayList<Product> getInventory();
+	
+	public boolean writeInventory(ArrayList<Product> list);
+	
 
 	public Employee getEmployee(int employeeId, String password);
+	
+	public void disconnect();
+
 }
---
+++ src/dao/DaoImplFile.java
@@ -0,0 +1,152 @@
+package dao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import model.Amount;
+import model.Employee;
+import model.Product;
+import model.Sale;
+
+public class DaoImplFile implements Dao {
+
+	ArrayList<Product> inventory = new ArrayList<Product>();
+
+	@Override
+	public void connect() {
+
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		// locate file, path and name
+		File f = new File(System.getProperty("user.dir") + File.separator + "files/inputInventory.txt");
+
+		try {
+			// wrap in proper classes
+			FileReader fr;
+			fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+
+			// read first line
+			String line = br.readLine();
+
+			// process and read next line until end of file
+			while (line != null) {
+				// split in sections
+				String[] sections = line.split(";");
+
+				String name = "";
+				double wholesalerPrice = 0.0;
+				int stock = 0;
+
+				// read each sections
+				for (int i = 0; i < sections.length; i++) {
+					// split data in key(0) and value(1)
+					String[] data = sections[i].split(":");
+
+					switch (i) {
+					case 0:
+						// format product name
+						name = data[1];
+						break;
+
+					case 1:
+						// format price
+						wholesalerPrice = Double.parseDouble(data[1]);
+						break;
+
+					case 2:
+						// format stock
+						stock = Integer.parseInt(data[1]);
+						break;
+
+					default:
+						break;
+					}
+
+				}
+
+				inventory.add(new Product(name, new Amount(wholesalerPrice), true, stock));
+				line = br.readLine();
+			}
+			fr.close();
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return inventory;
+	}
+
+	@Override
+	public boolean writeInventory(ArrayList<Product> inventory) {
+		// define file name based on date
+		LocalDate myObj = LocalDate.now();
+		String fileName = "inventory_" + myObj.toString() + ".txt";
+
+		// locate file, path and name
+		File f = new File(System.getProperty("user.dir") + File.separator + "files" + File.separator + fileName);
+
+		try {
+			// wrap in proper classes
+			FileWriter fw;
+			fw = new FileWriter(f, true);
+			PrintWriter pw = new PrintWriter(fw);
+
+			// write line by line
+			int counterProduct = 1;
+
+			for (Product product : inventory) {
+				StringBuilder firstLine = new StringBuilder(
+						counterProduct + ";Product:" + product.getName() + ";Stock=" + product.getStock() + ";");
+				pw.write(firstLine.toString());
+				fw.write("\n");
+
+				StringBuilder thirdLine = new StringBuilder(
+						"Total number of products:" + product.getTotalProducts() + ";");
+				pw.write(thirdLine.toString());
+				fw.write("\n");
+
+				counterProduct++;
+			}
+			// close files
+			pw.close();
+			fw.close();
+			
+			return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public Employee getEmployee(int employeeId, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void disconnect() {
+
+	}
+
+}
